5e94b3a7a94cda6c3df8d9c3a8a4874a011b28e5
MaShiChen <1934202608@qq.com>
2021-05-19 17:11:37 +0800

    UI设计修改

--- src/views/index.vue
+++ src/views/index.vue
@@ -9,36 +9,31 @@
               default-active="this.$route.path"
               mode="horizontal"
               :router="true"
+              
             >
               <el-menu-item index="/index/home">主页</el-menu-item>
               <el-menu-item index="/index/team">团队概况</el-menu-item>
               <el-menu-item index="/index/research">科研方向</el-menu-item>
-              <el-menu-item index="" disabled>科学研究</el-menu-item>
-              <el-menu-item index="" disabled>科研平台</el-menu-item>
+              <!-- <el-menu-item index="" disabled>科学研究</el-menu-item> -->
+              <!-- <el-menu-item index="" disabled>科研平台</el-menu-item> -->
               <el-menu-item index="/index/cooperation">交流合作</el-menu-item>
-              <el-menu-item index="" disabled>产学研用</el-menu-item>
-              <el-menu-item index="" disabled>资源共享</el-menu-item>
+              <!-- <el-menu-item index="" disabled>产学研用</el-menu-item> -->
+              <!-- <el-menu-item index="" disabled>资源共享</el-menu-item> -->
             </el-menu>
           </el-row>
         </el-col>
-        <el-col :span="3">
+        <!-- <el-col :span="3">
           <el-input placeholder="请输入内容" v-model="input4">
             <i slot="prefix" class="el-input__icon el-icon-search"></i>
           </el-input>
-        </el-col>
-        <el-col :span="2">
-          <!-- <el-avatar
-            @click="goToAdmin"
-            size="medium"
-            :src="circleUrl"
-            style="vertical-align: middle"
-          >
-          </el-avatar> -->
+        </el-col> -->
+        <el-col :span="2" :offset="3">
           <el-link :underline="false" @click="goToAdmin">admin</el-link>
         </el-col>
       </el-row>
     </el-header>
-    <el-main>
+    <el-main style="padding:0">
+      <div class="leftSide"></div>
       <router-view></router-view>
     </el-main>
     <el-footer>Footer 这里放脚注</el-footer>
@@ -53,7 +48,7 @@ export default {
     return {
       circleUrl:
         "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
-        input4:'placeholder'
+      input4: "placeholder",
     };
   },
   computed: {},
@@ -67,6 +62,7 @@ export default {
 </script>
 
 <style >
+
 .el-header {
   color: #333;
   text-align: center;
@@ -92,4 +88,20 @@ export default {
   text-align: center;
   min-height: 600px;
 }
+.leftSide {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100px;
+  border-right: 1px solid rgba(128, 128, 128, 0.231);
+  z-index: 100;
+}
+
+/* .el-menu-item{
+  font-weight: bolder;
+  font-size: 600;
+  font-family: MessinaSans,sans-serif;
+  
+} */
 </style>
--- src/views/team/team.vue
+++ src/views/team/team.vue
@@ -1,25 +1,79 @@
 <template>
   <div>
+    <!-- 面包屑 -->
     <el-row>
-      <el-col :span="2" :offset="2">
+      <el-col :span="18" :offset="3">
+        <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb">
+          <el-breadcrumb-item>首页</el-breadcrumb-item>
+          <el-breadcrumb-item>团队概况</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+    </el-row>
+
+    <!--文章 -->
+    <div class="article" v-for="(item, index) in listData" :key="item.id">
+      <el-row>
+        <el-col :span="18" :offset="3">
+          <!-- 标题 -->
+          <el-row>
+            <h2 :class="[index == 0 ? 'article_first_title' : 'article_title']">
+              {{ item.title }}
+            </h2>
+          </el-row>
+          <!-- 正文 -->
+          <el-row
+            :gutter="20"
+            type="flex"
+            justify="space-between"
+            style="margin-bottom: 20px"
+          >
+            <el-col :span="12">
+              <div v-if="item.description.length !== 0">
+                <p>{{ item.description }}</p>
+
+                <el-button @click="show3 = !show3" plain style="float:left">Click Me</el-button>
+
+                <div >
+                  <el-collapse-transition>
+                    <div v-show="show3">
+                      <div  v-html="item.content"></div>
+                    </div>
+                  </el-collapse-transition>
+                </div>
+              </div>
+              <div v-else v-html="item.content"></div>
+            </el-col>
+            <el-col :span="12">
+              <img
+                src="https://www.mit.edu/files/images/201807/MIT-Brain-Drug-Delivery-03.jpg"
+                alt=""
+                width="300px"
+              />
+              <video
+                src="https://www.mit.edu/files/images/201805/research-2_0.mp4"
+                muted
+                autoplay
+                loop
+                width="300px"
+              ></video>
+            </el-col>
+            <!-- 资源 -->
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+  <!-- <h2 class="left_title">
+        <span>团队概况</span>
+      </h2> -->
+  <!-- <el-col :span="2" :offset="2">
         <el-tabs tab-position="left" @tab-click="handleClick">
           <el-tab-pane label="团队简介" name="teamInfo"> </el-tab-pane>
           <el-tab-pane label="负责人简介" name="teamLeader"></el-tab-pane>
           <el-tab-pane label="团队成员" name="teamMember"> </el-tab-pane>
           <el-tab-pane label="团队文化" name="teamCulture"></el-tab-pane>
         </el-tabs>
-      </el-col>
-      <el-col :span="15" :offset="1">
-        <div class="breadcrumb">
-          <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item>团队概括</el-breadcrumb-item>
-            <el-breadcrumb-item>{{ kind }}</el-breadcrumb-item>
-          </el-breadcrumb>
-        </div>
-        <div v-html="content" class="notice"/>
-      </el-col>
-    </el-row>
-  </div>
+      </el-col> -->
 </template>
 
 <script>
@@ -31,6 +85,7 @@ export default {
       listData: [],
       content: "",
       kind: "",
+      show3: true,
     };
   },
   computed: {},
@@ -48,22 +103,78 @@ export default {
           },
         })
         .then((res) => {
-          this.listData = res.data;
-          this.content = this.listData[0].content;
+          this.listData.push(res.data[0]);
         });
     },
   },
-  mounted() {
-    this.getListData('teamInfo');
-    this.kind = '团队简介'
+  created() {
+    this.getListData("teamInfo");
+    this.getListData("teamLeader");
+    this.getListData("teamMember");
+    this.getListData("teamCulture");
+    this.listData.sort((item) => item.id);
+    console.log(this.listData);
+    // this.kind = '团队简介';
   },
 };
 </script>
 
-<style>
+<style scoped>
+.breadcrumb {
+  font-weight: bolder;
+  font-size: 15px;
+  margin-top: 80px;
+  margin-bottom: 50px;
+}
 
-.breadcrumb{
-  font-weight: bold;
-  margin: 20px 0 0 20px;
+.article {
+  border-bottom: 1px solid rgba(128, 128, 128, 0.231);
+  z-index: 100;
+}
+
+.article_first_title {
+  font-weight: 600;
+  line-height: 1.3;
+  font-size: 30px;
+  margin-top: -1px;
+  padding-bottom: 20px;
+  /* display: inline-block; */
+  float: left;
+  /* border-top: 3px solid rgba(31, 117, 221, 0.884); */
+}
+
+.article_title {
+  font-weight: 600;
+  line-height: 1.3;
+  font-size: 30px;
+  border-top: 3px solid rgba(31, 117, 221, 0.884);
+  margin-top: -1px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  /* display: inline-block; */
+  float: left;
+}
+.left_title {
+  position: absolute;
+  left: 10px;
+  top: 100px;
+  transform: rotate(90deg);
+  white-space: nowrap;
+  font-family: MessinaSans, sans-serif;
+  font-weight: bolder;
+  font: black;
+}
+
+.transition-box {
+  margin-bottom: 10px;
+  width: 200px;
+  height: 100px;
+  border-radius: 4px;
+  background-color: #409eff;
+  text-align: center;
+  color: #fff;
+  padding: 40px 20px;
+  box-sizing: border-box;
+  margin-right: 20px;
 }
 </style>
